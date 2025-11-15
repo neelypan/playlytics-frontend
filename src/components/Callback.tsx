2619fa123f0366b53e2c8f8cd0a174dcfb229a0b
Ayaan Gill <ayaan.gill@mcvts.org>
2025-11-15 15:50:24 -0500

    tried to create callback

--- src/components/Callback.tsx
+++ src/components/Callback.tsx
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
+import { useEffect } from "react";
 
-const Callback = () => {
-  return <div></div>;
-};
+export default function Callback() {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+    const state = params.get("state");
 
-export default Callback;
+    if (!code || !state) return;
+
+    fetch("http://localhost:8000/api/auth/exchange", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code, state })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("Tokens:", data);
+      })
+      .catch(() => console.log("Exchange failed"));
+  }, []);
+
+  return <div>Processing login…</div>;
+}
+
